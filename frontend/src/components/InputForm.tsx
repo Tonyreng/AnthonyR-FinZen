7c6fb0652d1ba6eb6b657812e5fc002b871a6e54
Anthony Rengifo <tonyreng01@gmail.com>
2025-10-13 13:17:30 -0500

    The InputForm component was created to be used later on the Login and Signup pages.

---
+++ frontend/src/components/InputForm.tsx
@@ -0,0 +1,51 @@
+import { Checkbox, FormControlLabel, TextField } from '@mui/material';
+import { Controller, useFormContext } from 'react-hook-form';
+
+type Props = {
+    name: string;
+    rules?: object;
+    label?: string;
+    type?: string;
+    styleInput?: object;
+};
+
+const InputForm = ({ name, rules, label, type, styleInput }: Props) => {
+    const { control, formState, getFieldState } = useFormContext();
+    const { error } = getFieldState(name, formState);
+
+    return (
+        <Controller
+            name={name}
+            control={control}
+            rules={rules && { ...rules }}
+            render={({ field }) =>
+                type === 'textfield' ? (
+                    <TextField
+                        {...field}
+                        label={label}
+                        variant="outlined"
+                        fullWidth
+                        required
+                        error={!!error}
+                        helperText={error && error?.message}
+                    />
+                ) : (
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                {...field}
+                                size="small"
+                                sx={{ color: 'primary.main' }}
+                                checked={field.value}
+                            />
+                        }
+                        label={label}
+                        sx={styleInput}
+                    />
+                )
+            }
+        />
+    );
+};
+
+export default InputForm;
