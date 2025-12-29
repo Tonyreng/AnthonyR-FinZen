bf4327d3d32a57f3534ebf1d65ff9a94426d86af
Anthony Rengifo <tonyreng01@gmail.com>
2025-12-29 06:02:52 -0500

    The Dashboard page was added to the routes

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -7,12 +7,14 @@ import { Layout } from './pages/Layout';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import { ErrorDetail } from './pages/ErrorDetail';
+import { Dashboard } from './pages/Dashboard';
 
 export const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<Layout />} errorElement={<ErrorDetail />}>
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
         </Route>
     )
 );
