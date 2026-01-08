4524abcc4716cf9f2eb439f698f8aff1e1d67089
Anthony Rengifo <tonyreng01@gmail.com>
2026-01-08 08:23:10 -0500

    Dashboard bp was initialized

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -1,8 +1,10 @@
 # Routes package
 from .login_route import login_bp
 from .refresh_route import refresh_bp
+from .dashboard_route import dashboard_bp
 
 
 def register_routes(app):
     app.register_blueprint(login_bp, url_prefix='/api')
     app.register_blueprint(refresh_bp, url_prefix='/api')
+    app.register_blueprint(dashboard_bp, url_prefix='/api')
