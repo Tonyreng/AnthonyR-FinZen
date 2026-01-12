b342e99de03b6c93bdfd7382b5a6b34a66676b7c
Anthony Rengifo <tonyreng01@gmail.com>
2026-01-11 21:02:26 -0500

    The useDashboard hook was imported and the total balance was passed as a prop to the CardBalance component.

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,7 +1,10 @@
 import FormHeader from '../components/FormHeader';
 import { CardBalance } from '../components/CardBalance';
+import { SummaryCardGroup } from '../components/SummaryCardGroup';
+import { useDashboard } from '../hooks/useDashboard';
 
 export const Dashboard = () => {
+    const { data, error, isLoading } = useDashboard();
     return (
         <>
             <FormHeader
@@ -17,7 +20,9 @@ export const Dashboard = () => {
                     px: 3,
                     mt: 3,
                 }}
+                totalBalance={data?.total_balance}
             />
+            <SummaryCardGroup />
         </>
     );
 };
