f9a808184aeafe72068d5f234e53bbf254df9137
Anthony Rengifo <tonyreng01@gmail.com>
2026-02-14 12:19:01 -0500

    Register accounts route in the application

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -2,9 +2,11 @@
 from .login_route import login_bp
 from .refresh_route import refresh_bp
 from .dashboard_route import dashboard_bp
+from .accounts_route import accounts_bp
 
 
 def register_routes(app):
     app.register_blueprint(login_bp, url_prefix='/api')
     app.register_blueprint(refresh_bp, url_prefix='/api')
     app.register_blueprint(dashboard_bp, url_prefix='/api')
+    app.register_blueprint(accounts_bp, url_prefix='/api')
